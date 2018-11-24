8fd2b5fc5b06ca7e5343942b58ce5ec963f111fc
vkuzugashev <vkuzugashev@mail.ru>
2018-11-24 13:36:18 +0700

    внес поправки

--- src/ITSPS/SmRecordBundle/Controller/DefaultController.php
+++ src/ITSPS/SmRecordBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use ITSPS\SmRecordBundle\Entity\Record;
+use ITSPS\SmRecordBundle\Entity\Loc;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
@@ -272,12 +273,11 @@ class DefaultController extends Controller
         //Проверим что разрешено загружать запись
         if(count($phonerecord) > 0) {
            $row=array(
-                'isrec'=>$phonerecord[0]->getIsrec(),
-                'hourfrom'=>$phonerecord[0]->getHourfrom(),
-                'hourto'=>$phonerecord[0]->getHourto(),
+                'isrec' => $phonerecord[0]->getIsrec(),
+                'hourfrom' => $phonerecord[0]->getHourfrom(),
+                'hourto' => $phonerecord[0]->getHourto()
             );
             $response = new JsonResponse();
-            //$response->setData(array('row'=>$row, 'error'=>false));
 	    $response->setData($row);
             return $response;                
         } else {
@@ -287,5 +287,97 @@ class DefaultController extends Controller
 	    return $response; 
         }
     }    
+
+
+    /**
+     * @Route("/smrecord/loc", name="loc")
+     * @Method({"POST"})
+     */
+    public function locAction(Request $request)
+    {   
+        $logger = $this->get('logger');
+
+        $logger->warn('PHONE=' . $request->request->get('PHONE'));
+        $logger->warn('LAT=' . $request->request->get('LAT'));
+        $logger->warn('LAT=' . $request->request->get('LON'));
+
+        $dt = $request->request->get('DT');
+        $phone = $request->request->get('PHONE');
+        $lat = $request->request->get('LAT');
+	$lat = str_replace(',', '.', $lat);
+        $lon = $request->request->get('LON');
+	$lon = str_replace(',', '.', $lon);
+
+
+        $em =  $this->getDoctrine()->getManager();
+        $phonerecord = $em->getRepository("SmRecordBundle:UserPhone")->findByPhone($phone);
+
+        //Проверим что разрешено загружать запись        
+        if(count($phonerecord) > 0){
+            $userid=$phonerecord[0]->getUserid();
+            if($dt!="0"){
+                try{
+                    $row = new Loc();        
+                    $row->setDt(\DateTime::createFromFormat('YmdHis', $dt));
+                    $row->setLat(floatval($lat));
+                    $row->setLon(floatval($lon));
+                    $row->setPhone($phone);
+                    $row->setUserid($userid);
+                    $em->persist($row);
+                    $em->flush();     
+                }
+                catch(\Exception $ex){
+                    $logger->error('Loc not saved: '.$ex->getMessage());
+                }
+            }
+            else{
+                $logger->warn('Uncknow format date: '.$startdt);
+            }
+        }
+        else
+            $logger->warn('Not registered phone: '.$phone);
+        
+        $response = new JsonResponse();
+        return $response; 
+    }
+
+    /**
+     * @Route("/smrecord/getloc", name="getloc")
+     * @Method({"GET"})
+     */
+    public function getLocAction(Request $request)
+    {   
+        $logger = $this->get('logger');
+
+        $logger->warn('PHONE=' . $request->get('PHONE'));
+
+        $dt = $request->get('DT');
+        $phone = $request->get('PHONE');
+
+        $em =  $this->getDoctrine()->getManager();
+        $query = $em->createQuery('select o from SmRecordBundle:Loc o '
+                    . 'where o.dt between :p1 and :p2 and o.phone = :p3 order by o.dt');
+        $query->setParameter('p1',\DateTime::createFromFormat('Y-m-d H:i:s', $dt.' 00:00:00'));
+        $query->setParameter('p2',\DateTime::createFromFormat('Y-m-d H:i:s', $dt.' 00:00:00')->add(new \DateInterval('P1D')));
+        $query->setParameter('p3', $phone);
+        $logger->warn('SQL: '.$query->getSql());
+        $locs = $query->getResult();
+
+        $rows = array();
+        foreach($locs as $loc){
+		$rows[]=array(
+			'DT'=>$loc->getDt()->format('d.m.Y H:i:s'),
+			'LAT'=>$loc->getLat(),
+			'LON'=>$loc->getLon()
+	        );
+	}
+
+        $response = new JsonResponse();
+	$response->setData($rows);
+        return $response; 
+    }
+
+
     
 }
+
---
+++ src/ITSPS/SmRecordBundle/Entity/Loc.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace ITSPS\SmRecordBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Loc
+ *
+ * @ORM\Table(name="Loc")
+ * @ORM\Entity
+ */
+class Loc
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=15)
+     */
+    private $phone;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dt", type="datetime")
+     */
+    private $dt;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lat", type="float")
+     */
+    private $lat;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lon", type="float")
+     */
+    private $lon;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="userid", type="integer", nullable=true)
+     */
+    private $userid;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set dt
+     *
+     * @param \DateTime $dt
+     * @return Loc
+     */
+    public function setDt($dt)
+    {
+        $this->dt = $dt;
+
+        return $this;
+    }
+
+    /**
+     * Get dt
+     *
+     * @return \DateTime 
+     */
+    public function getDt()
+    {
+        return $this->dt;
+    }
+
+    /**
+     * Set lat
+     *
+     * @param float $lat
+     * @return Loc
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    /**
+     * Get lat
+     *
+     * @return float 
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * Set lon
+     *
+     * @param float $lon
+     * @return Loc
+     */
+    public function setLon($lon)
+    {
+        $this->lon = $lon;
+
+        return $this;
+    }
+
+    /**
+     * Get lon
+     *
+     * @return float 
+     */
+    public function getLon()
+    {
+        return $this->lon;
+    }
+
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     * @return Loc
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string 
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set userid
+     *
+     * @param integer $userid
+     * @return Record
+     */
+    public function setUserid($userid)
+    {
+        $this->userid = $userid;
+
+        return $this;
+    }
+
+    /**
+     * Get userid
+     *
+     * @return integer 
+     */
+    public function getUserid()
+    {
+        return $this->userid;
+    }
+}
